fcb92889e37075e3ba18e52c8349e189509d786a
Stanislas Ormières <stan@stormier.ninja>
2024-07-08 16:10:05 +0200

    refactor: ♻️ use fastify instead of express

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-import express from 'express'
+import Fastify from 'fastify'
 import dotenv from 'dotenv'
 
 // Load the environment variables
 dotenv.config()
 
 const port = 3010
-const hostname = '0.0.0.0'
+const host = '0.0.0.0'
 
 // Get API key from env vars
 const apiKey = process.env.OMDB_API_KEY
@@ -15,22 +15,28 @@ const baseUrl = 'http://www.omdbapi.com/'
 const apiUrl = `${baseUrl}?apikey=${apiKey}`
 const searchUrl = `${apiUrl}&s=`
 
-const app = express()
+const app = Fastify({
+  logger: true
+})
 
-app.get('/media', async (req, res) => {
+app.get('/media', async (req, reply) => {
   const search = req.query.s
-  // // Create the url to make the request to
+  // Create the url to make the request to
   const body = await fetch(searchUrl + search).then(res => res.json())
   const titles = body.Search.map((el) => {
     return `${el.Title} (${el.Year})`
   })
-  res.json(titles) // By default, express sends the 200 status code
+  reply.status(200)
+  return titles // By default, fastify sends the 200 status code
 })
 
 app.get('/', async (req, res) => {
-  res.json({ message: 'Hello World' }) // By default, express sends the 200 status code
+  res.json({ message: 'Hello World' })
 })
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+try {
+  await app.listen({ port, host })
+} catch (err) {
+  app.log.error(err)
+  process.exit(1)
+}
